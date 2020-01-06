5696fd166280115cbd84f6504fd4912e94755240
higsch <matthias.stahl87@web.de>
2020-01-06 13:22:49 +0100

    highlighter animations

--- src/flower/CountryHighlighter.svelte
+++ src/flower/CountryHighlighter.svelte
@@ -12,46 +12,85 @@
   export let selectedIso;
 
   let reduction;
-  let container, reductionCircle;
+  let container;
+  let modelYears, modelReduction;
 
-  function update() {
-    reduction = data.find(d => d.iso === selectedIso).reduction || 0;
+  function update(selectedIso) {
+    if (!selectedIso) {
+      modelYears = [];
+      reduction = 0;
+      modelReduction = [];
+    } else {
+      reduction = data.find(d => d.iso === selectedIso).reduction;
+      modelReduction = [{
+        cx: Math.sin(Math.PI - scCountryAngle(selectedIso)) * scReduction(reduction) * (reduction <= 0 ? 0.97 : 1.03),
+        cy: Math.cos(Math.PI - scCountryAngle(selectedIso)) * scReduction(reduction) * (reduction <= 0 ? 0.97 : 1.03),
+        r: Math.min(width, height) / 200
+      }];
+      modelYears = data.find(d => d.iso === selectedIso).dataArr.filter(d => years.includes(d.year)) || [];
+    }
 
+    // the three year highlighters
     d3.select(container).selectAll('.year-circle')
-      .data(data.find(d => d.iso === selectedIso).dataArr.filter(d => years.includes(d.year)))
-      .join('circle')
-        .attr('class', 'year-circle')
-        .attr('fill', 'white')
-        .attr('opacity', 0.4)
-        .transition().duration(100)
-          .attr('cx', d => Math.sin(Math.PI - scCountryAngle(selectedIso)) * scYearRadius(d.year))
-          .attr('cy', d => Math.cos(Math.PI - scCountryAngle(selectedIso)) * scYearRadius(d.year))
-          .attr('r', d => scMortRate(d.value));
+      .data(modelYears)
+      .join(enter => enter.append('circle')
+                      .attr('class', 'year-circle')
+                      .attr('fill', 'white')
+                      .attr('opacity', 0.4)
+                      .attr('cx', 0)
+                      .attr('cy', 0)
+                      .attr('r', 0)
+                      .call(enter => enter.transition().duration(100)
+                        .attr('cx', d => Math.sin(Math.PI - scCountryAngle(selectedIso)) * scYearRadius(d.year))
+                        .attr('cy', d => Math.cos(Math.PI - scCountryAngle(selectedIso)) * scYearRadius(d.year))
+                        .attr('r', d => scMortRate(d.value))),
+            update => update.transition().duration(100)
+                        .attr('cx', d => Math.sin(Math.PI - scCountryAngle(selectedIso)) * scYearRadius(d.year))
+                        .attr('cy', d => Math.cos(Math.PI - scCountryAngle(selectedIso)) * scYearRadius(d.year))
+                        .attr('r', d => scMortRate(d.value)),
+            exit => exit.transition().duration(100)
+                      .attr('cx', 0)
+                      .attr('cy', 0)
+                      .attr('r', 0)
+                      .remove()
+      );
 
-    d3.select(reductionCircle)
-      .transition().duration(100)
-      .attr('cx', Math.sin(Math.PI - scCountryAngle(selectedIso)) * scReduction(reduction) * (reduction <= 0 ? 0.97 : 1.03))
-      .attr('cy', Math.cos(Math.PI - scCountryAngle(selectedIso)) * scReduction(reduction) * (reduction <= 0 ? 0.97 : 1.03));
+    // the reduction highlighter
+    d3.select(container).selectAll('.reduction-circle')
+      .data(modelReduction)
+      .join(enter => enter.append('circle')
+                      .attr('class', `reduction-circle ${reduction <= 0 ? 'decreased' : 'increased'}`)
+                      .attr('cx', 0)
+                      .attr('cy', 0)
+                      .attr('r', 0)
+                      .call(enter => enter.transition().duration(100)
+                        .attr('cx', d => d.cx)
+                        .attr('cy', d => d.cy)
+                        .attr('r', d => d.r)),
+            update => update.transition().duration(100)
+                        .attr('cx', d => d.cx)
+                        .attr('cy', d => d.cy)
+                        .attr('r', d => d.r),
+            exit => exit.transition().duration(100)
+                      .attr('cx', 0)
+                      .attr('cy', 0)
+                      .attr('r', 0)
+                      .remove()
+      );
   }
 
-  $: if (selectedIso && reductionCircle) update();
+  $: if (container) update(selectedIso);
 </script>
 
-{#if selectedIso}
-  <g transform="translate({width / 2} {height / 2})" bind:this={container}>
-    <circle bind:this={reductionCircle}
-            class={reduction <= 0 ? 'decreased' : 'increased'}
-            r={Math.min(width, height) / 200}></circle>
-  </g>
-{/if}
+<g transform="translate({width / 2} {height / 2})" bind:this={container}></g>
 
 <style>
-  circle.increased {
+  :global(circle.increased) {
     opacity: 1;
     fill: var(--red);
   }
 
-  circle.decreased {
+  :global(circle.decreased) {
     opacity: 1;
     fill: var(--blue);
   }
